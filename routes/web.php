9378956350e72e6db68d163f4b51d5c22a9bf3ab
Abd alashqr <mr.abosak@gmail.com>
2020-09-05 22:20:29 +0300

    Add Template

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('welcome');});
+
+
+//Route::get('/test',function (){
+//    return view('test');
+//});
+//
+
 
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('users', 'UserController');
 Route::resource('posts', 'PostController');
